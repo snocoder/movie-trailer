a8e323a61a722b703583b1c3a39c0ab3c08e597e
Ankit Kumar <ankit230698@gmail.com>
2019-09-18 21:34:51 +0530

    First

---
+++ getTrailer.js
@@ -0,0 +1,29 @@
+const request = require('request-promise')
+
+const baseURL = 'https://api.themoviedb.org/3'
+const apiKey = 'api_key=c7030e7145a9c11ff588a71792ab3020'
+
+const getOptions = url => {
+    return {
+        method: 'GET',
+        url: url,
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.75 Safari/537.36'
+        }
+    }
+}
+
+
+
+const sendTrailer = async (trailerName) => {
+    const searchUrl = `${baseURL}/search/movie?${apiKey}&query=${trailerName}`
+    const searchRes = await request.get(getOptions(searchUrl))
+    const movieId = JSON.parse(searchRes).results[0].id
+    const traileUrl = `${baseURL}/movie/${movieId}/videos?${apiKey}`
+    const vRes = await request.get(getOptions(traileUrl))
+    const youtubeId = JSON.parse(vRes).results[0].key
+    // console.log(youtubeId)
+    return youtubeId
+}
+
+module.exports = sendTrailer
---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser');
+const sendTrailer = require('./getTrailer')
+
+
+
+const app = express();
+app.use(bodyParser.raw());
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+    console.log(req.query)
+    res.render('index')
+});
+
+
+// app.get()
+
+app.post('/getTrailer', async (req, res) => {
+    const body = req.body;
+    const trailerName = body.trailer
+    if(trailerName){
+        sendTrailer(trailerName).then(youTubeId => {
+            console.log(youTubeId)
+            url = `https://www.youtube.com/watch?v=${youTubeId}`
+            res.render('video', {id: youTubeId, url: url})
+        }) 
+    }
+    else{
+        res.send('Invalid TrailerName')
+    }
+    // res.send('snocoder')
+
+});
+
+
+// app.get('/getTrailer', async (req, res) => {
+//     const body = req.params;
+//     console.log(body)
+//     const trailerName = body.trailer
+//     if(trailerName){
+//         sendTrailer(trailerName).then(youTubeId => {
+//             console.log(youTubeId)
+//             res.render('video', {id: youTubeId})
+//         }) 
+//     }
+//     else{
+//         res.send('Invalid TrailerName')
+//     }
+//     // res.send('snocoder')
+
+// });
+
+app.listen(3000, () => console.log('Listening on port 3000..'))
+
+
+// AIzaSyBJUgsn9cYSQcGvSjOTb__VE9fYQtXv1bk
